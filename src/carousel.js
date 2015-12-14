5fbe1dae8a2e6b009479eef63fdcad97875d8504
amir farzan <amir.ff@gmail.com>
2015-12-13 18:02:40 -0800

    More comments.

--- src/carousel.js
+++ src/carousel.js
@@ -6,7 +6,10 @@ $(document).ready(function() {
   // Image collection.
   var carouselImages = [
     "src/img/img1.jpg",
-
+    "src/img/img2.jpg",
+    "src/img/img3.jpg",
+    "src/img/img4.jpg",
+    "src/img/img5.jpg"
   ];
 
   var totalCarouselImages = carouselImages.length;
@@ -25,14 +28,15 @@ $(document).ready(function() {
     var imgLeftSrc;
     var imgElements;
 
-    // If at start, set the last image in the collection as the left image.
+    /** If at start, set the last image in the collection as the left/previous  * image.
+    */
     if (activeImgIndex === 0) {
       imgLeftSrc = carouselImages[totalCarouselImages - 1];
     } else {
       imgLeftSrc = carouselImages[activeImgIndex - 1];
     }
 
-    // If at end, set the first image in the collection as the right image.
+    // If at end, set the first image in the collection as the right/next image.
     if (activeImgIndex === totalCarouselImages - 1) {
       imgRightSrc = carouselImages[0];
     } else {
@@ -50,6 +54,12 @@ $(document).ready(function() {
   // Start carousel on page load.
   $('.carousel').html( carouselInnerHTML(0) );
 
+  /**
+  * Depending on advance direction, get index for generating the next set of    * three images.
+  *
+  * @param {String} advanceDirection
+  * @return {Integer} nextIndex
+  */
   function nextThree(advanceDirection) {
 
     var currentActiveIndex = $('.img-active').data('active-index');
@@ -66,17 +76,20 @@ $(document).ready(function() {
     return nextIndex;
   }
 
+  // Advancing slides right.
   function advanceRight() {
     $('.advance-right').unbind('click');
     $('.img-active').addClass('slide-out-left');
     $('.img-right').addClass('slide-in-left');
 
+    // Wait for CSS animation to end, before getting new images.
     $('.slide-out-left').on('animationend', function() {
       $('.carousel').html( carouselInnerHTML( nextThree('right') ) );
       $('.advance-right').bind('click', advanceRight);
     });
   }
 
+  // Advance Left.
   function advanceLeft() {
     $('.advance-left').unbind('click');
     $('.img-active').addClass('slide-out-right');
